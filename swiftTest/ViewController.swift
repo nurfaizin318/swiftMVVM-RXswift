d3551a1c73ecbaf4a23b972c0883c4726f54c504
Faiz <nurfaizin318@gmail.com>
2023-01-17 10:24:26 +0700

    Initial Commit

---
+++ swiftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftTest
+//
+//  Created by MacBook on 17/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
